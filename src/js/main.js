040ae44f3ae0ecde61113fdef6ed60c30d73fb43
Swan Frere <swan.frere@idci-consulting.fr>
2022-10-30 11:16:09 +0100

    Add: Gsap

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,26 @@
 gsap.registerPlugin(ScrollTrigger);
 
-// gsap.to(".scroll-downs", {
-//     x: 200000,
-//     duration: 0.0001,
-//     scrollTrigger: {
-//       trigger: ".scroll-downs",
-//       start: "+=100 +=600",
-//       markers: true
-//     }
-// });
-
-const tl = gsap.timeline({ 
+const mouseIconTl = gsap.timeline({ 
     paused: true, 
     defaults: { duration: 2 },
     scrollTrigger: {
         trigger: ".scroll-downs",
         start: "+=100 +=600",
-        markers: true
       }
 })
-.to(".scroll-downs", { opacity: 0 })
+.to(".scroll-downs", { opacity: 0 });
+
+const ResumeTl = gsap.timeline({ 
+    paused: true, 
+    //defaults: { duration: 2 },
+    scrollTrigger: {
+        trigger: "#it1",
+        start: "top center",
+        //markers: true
+      }
+})
+.from("#it2", { opacity: 0, x: 500, duration: 1 })
+.from("#it1", { opacity: 0, x: -500, duration: 1})
+.from("#it3", { opacity: 0, x: -500, duration: 1 })
+.from("#it4", { opacity: 0, x: 500, duration: 1 })
+.from("#it5", { opacity: 0, x: 500, duration: 1 });
